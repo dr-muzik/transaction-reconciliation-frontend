fd664ee8cf1f2d8b8848efb41bfd379056954072
Wisdom Peters <drmusicpiano@gmail.com>
2025-05-05 11:23:21 +0100

    feat: result summary was the bug that was causing the bug

--- src/components/ResultSummary.tsx
+++ src/components/ResultSummary.tsx
@@ -22,7 +22,7 @@ interface FormattedData {
   }[];
 }
 const ResultSummary: React.FC = () => {
-  const [data, setData] = useState<any>(null);
+  // const [data, setData] = useState<any>(null);
 
   const [formattedData, setFormattedData] = useState<FormattedData | null>(
     null
@@ -31,53 +31,62 @@ const ResultSummary: React.FC = () => {
   useEffect(() => {
     const stored = localStorage.getItem("reconciliation");
     console.log({ stored });
-    if (stored) setData(JSON.parse(stored));
-
-    const { details } = stored ? JSON.parse(stored) : {};
-    const mappedData = {
-      missingInA: details.missingInA.map((id: string) => ({
-        transactionId: id,
-        amount: "-",
-        status: "-",
-        source: "B", // present in B but not in A
-      })),
-      missingInB: details.missingInB.map((id: string) => ({
-        transactionId: id,
-        amount: "-",
-        status: "-",
-        source: "A", // present in A but not in B
-      })),
-      mismatched: [
-        ...details.amountMismatches.map((id: string) => ({
-          transactionId: id,
-          amount: "Mismatch",
-          status: "-",
-          source: "Both",
-        })),
-        ...details.statusMismatches.map((id: string) => ({
+    // if (stored) setData(JSON.parse(stored));
+    if (stored) {
+      const { details } = stored ? JSON.parse(stored) : {};
+      const mappedData = {
+        missingInA: details?.missingInA.map((id: string) => ({
           transactionId: id,
           amount: "-",
-          status: "Mismatch",
-          source: "Both",
+          status: "-",
+          source: "B", // present in B but not in A
         })),
-      ].sort((a: any, b: any) => {
-        const numA = parseInt(a.transactionId.replace("tx", ""));
-        const numB = parseInt(b.transactionId.replace("tx", ""));
-        return numA - numB;
-      }),
-    };
-    try {
+        missingInB: details?.missingInB.map((id: string) => ({
+          transactionId: id,
+          amount: "-",
+          status: "-",
+          source: "A", // present in A but not in B
+        })),
+        mismatched: [
+          ...details?.amountMismatches.map((id: string) => ({
+            transactionId: id,
+            amount: "Mismatch",
+            status: "-",
+            source: "Both",
+          })),
+          ...details.statusMismatches.map((id: string) => ({
+            transactionId: id,
+            amount: "-",
+            status: "Mismatch",
+            source: "Both",
+          })),
+        ].sort((a: any, b: any) => {
+          const numA = parseInt(a.transactionId.replace("tx", ""));
+          const numB = parseInt(b.transactionId.replace("tx", ""));
+          return numA - numB;
+        }),
+      };
+
+      // if (!mappedData) throw new Error("No data found in local storage.");
       setFormattedData(mappedData);
-    } catch (error) {
-      console.error("Failed to fetch results:", error);
+    } else {
+      console.log("No data found in local storage.");
     }
 
     // fetchResults();
   }, []);
 
-  if (!formattedData) return <p className="p-4">Loading summary...</p>;
+  // if (!formattedData) return <p className="p-4">Loading summary...</p>;
 
-  if (!data) return <p>No data found. Please upload files first.</p>;
+  if (!formattedData)
+    return (
+      <div className="">
+        <h2 className="text-2xl mt-5 mb-10  text-black font-bold">
+          Report Summary
+        </h2>
+        <p className="text-black">No data found. Please upload files first.</p>;
+      </div>
+    );
 
   return (
     <div className="">
